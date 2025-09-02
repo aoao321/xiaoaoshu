5870234c31104be5746f759a649a02f59762227f
Aoao <931203821@qq.com>
2025-09-02 19:29:45 +0800

    feat: 完成nacos配置

--- xiaoaoshu-auth/src/main/java/com/aoao/xiaoaoshu/auth/XiaoaoshuAuthApplication.java
+++ xiaoaoshu-auth/src/main/java/com/aoao/xiaoaoshu/auth/XiaoaoshuAuthApplication.java
@@ -4,12 +4,14 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @EnableConfigurationProperties
 @EnableAspectJAutoProxy(exposeProxy = true)
 @MapperScan("com.aoao.xiaoaoshu.auth.domain.mapper")
+@EnableDiscoveryClient
 public class XiaoaoshuAuthApplication {
     public static void main(String[] args) {SpringApplication.run(XiaoaoshuAuthApplication.class, args);}
 
