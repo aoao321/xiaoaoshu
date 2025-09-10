a39030a6929a277b1e4d6a135eaaf3cd87a25285
Aoao <931203821@qq.com>
2025-09-10 18:21:36 +0800

    feat: 完成用户模块搭建

---
+++ xiaoaoshu-user/xiaoaoshu-user-biz/src/main/java/com/aoao/xiaoaoshu/user/biz/XiaoaoshuUserBizApplication.java
@@ -0,0 +1,15 @@
+package com.aoao.xiaoaoshu.user.biz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author aoao
+ * @create 2025-09-10-18:20
+ */
+@SpringBootApplication
+public class XiaoaoshuUserBizApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(XiaoaoshuUserBizApplication.class, args);
+    }
+}
