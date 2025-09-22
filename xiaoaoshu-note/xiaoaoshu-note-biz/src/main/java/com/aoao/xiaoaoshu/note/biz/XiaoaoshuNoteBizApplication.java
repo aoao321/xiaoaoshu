96df0d4af8bd3f3ed27e84fbd31aea9e1d55f5cb
Aoao <931203821@qq.com>
2025-09-22 15:28:45 +0800

    feat: 搭建note模块

---
+++ xiaoaoshu-note/xiaoaoshu-note-biz/src/main/java/com/aoao/xiaoaoshu/note/biz/XiaoaoshuNoteBizApplication.java
@@ -0,0 +1,15 @@
+package com.aoao.xiaoaoshu.note.biz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author aoao
+ * @create 2025-09-22-15:20
+ */
+@SpringBootApplication
+public class XiaoaoshuNoteBizApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(XiaoaoshuNoteBizApplication.class, args);
+    }
+}
